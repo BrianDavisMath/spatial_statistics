3229d54a5d6142b6d38262bac1738ad3bf167e0a
briansdumb <bevo.davis@gmail.com>
2019-11-02 14:35:03 -0400

    new metric

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 # spatial_statistics
 A repo for computing spatial statistics of training / validation splits
-of binary labeled data sets.
+of binary-labeled data sets.
 
 ## Usage
   ```
-  uky_score, validation_weights = get_weights_and_bias(features, is_bool_feature, labels, is_validation, parallel=True)
-  ```
+import numpy as np
+from spatial_statistics import get_weights_and_bias
+
+# synthetic data
+data_size = 500
+continuous_feature_dim = 3
+binary_features_dim = 10
+balance = 0.25
+labels = np.random.choice(2, data_size, p=(1-balance, balance)).astype(bool)
+continuous_features = np.random.sample((data_size, continuous_feature_dim))
+binary_features = np.random.choice(2, size=(data_size, binary_features_dim)).astype(bool)
+features = np.hstack([binary_features, continuous_features])
+bool_features = np.hstack([np.ones(binary_features_dim), np.zeros(continuous_feature_dim)]).astype(bool)
+is_training = np.random.choice(data_size, size=int(data_size * 0.8), replace=False)
+is_validation = np.array([index not in is_training for index in range(data_size)])
+
+validation_weights, uky_score = get_weights_and_bias(features, labels, is_validation, is_bool_feature=bool_features)
+```
+
+
+Keyword arguments for ```spatial_statistics.get_weights_and_bias``` include:
+   * ```cat_weight``` for the weighting (between 0 and 1) for the binary features in the distance computation. The default value is the proportion of features that are binary.
+   * ```parallel``` for using ```pathos.multiprocessing.ProcessingPool``` to parallelize the distance computations. Default is ```False```.
+   * ```is_bool_feature``` for declaring which features are binary. Default is ```None```.
--- spatial_statistics.py
+++ spatial_statistics.py
@@ -1,99 +1,110 @@
 import numpy as np
-from sklearn.metrics import pairwise_distances, pairwise_distances_argmin_min
+from sklearn.metrics import pairwise_distances
 from scipy.sparse import csr_matrix
 from pathos.multiprocessing import ProcessingPool as Pool
 
 
-def mixed_metric(features_1, features_2, is_bool_feature=None):
+def mixed_metric(features_1_, features_2_, cat_weight_, bool_features_=None):
     """
     A distance function that uses Jaccard distance for binary features and Euclidean distance for continuous features.
     The mixed metric is the root of sum of squares of the distance on the binary and continuous features.
-    :param features_1: 2D numpy array of features
-    :param features_2: 2D numpy array of features
-    :param is_bool_feature: 1D numpy boolean array indexing which features are bool type (fingerprints)
+    :param features_1_: 1D numpy array of features
+    :param features_2_: 1D numpy array of features
+    :param cat_weight_: Scalar between 0 and 1 for the amount to weight the Jaccard distance on binary features
+    :param bool_features_: 1D numpy boolean array indexing which features are bool type (fingerprints)
     :return: 1D numpy array returning distances from rows of features_1 to rows of features_2
     """
-    if not is_bool_feature:
-        "If no binary features are flagged, assume all features are continuous."
-        *_, dim = features_1.shape
-        is_bool_feature = np.zeros(dim).astype(bool)
-    if features_1.ndim == 1:
-        features_1_ = features_1.reshape(1, -1)
-    if features_2.ndim == 1:
-        features_2_ = features_2.reshape(1, -1)
-    categorical_features_1 = features_1[:, is_bool_feature].astype(bool)
-    continuous_features_1 = features_1[:, ~is_bool_feature]
-    categorical_features_2 = features_2[:, is_bool_feature].astype(bool)
-    continuous_features_2 = features_2[:, ~is_bool_feature]
-    cat_distances = pairwise_distances(categorical_features_1, categorical_features_2, metric="jaccard")
-    cont_distances = pairwise_distances(continuous_features_1, continuous_features_2, metric="euclidean")
-    hybrid_dist = np.sqrt(np.square(cat_distances) + np.square(cont_distances))
-    return hybrid_dist
+    if not np.any(bool_features_):
+        "If no binary features are flagged, or if none explicitly flagged:"
+        return pairwise_distances(features_1_.reshape(1, -1), features_2_.reshape(1, -1), metric="euclidean")
+    elif np.all(bool_features_):
+        "If all features explicitly flagged as binary:"
+        return pairwise_distances(features_1_.reshape(1, -1), features_2_.reshape(1, -1), metric="jaccard")
+    else:
+        "If both binary / continuous features are flagged as present:"
+        if (features_1_.ndim != 1) or (features_2_.ndim != 1):
+            raise ValueError("Input to mixed_metric should be 1D array.")
+        categorical_features_1 = features_1_[bool_features_].astype(bool).reshape(1, -1)
+        continuous_features_1 = features_1_[~bool_features_].reshape(1, -1)
+        categorical_features_2 = features_2_[bool_features_].astype(bool).reshape(1, -1)
+        continuous_features_2 = features_2_[~bool_features_].reshape(1, -1)
+        cat_distances = cat_weight_ * pairwise_distances(categorical_features_1, categorical_features_2,
+                                                        metric="jaccard")
+        cont_distances = (1 - cat_weight_) * pairwise_distances(continuous_features_1, continuous_features_2,
+                                                               metric="euclidean")
+        hybrid_dist = np.sqrt(np.square(cat_distances) + np.square(cont_distances))[0, 0]
+        return hybrid_dist
 
 
-def min_distances_parallel(X, Y, bool_mask):
-    """
-    Parallel computation of minimum distances.
-    :param X: 2D numpy array of features
-    :param Y: 2D numpy array of features
-    :param bool_mask: 1D numpy boolean array indexing which features are bool type (fingerprints)
-    :return numpy array of minimum distance from each row of X to any row of Y
-    """
-    def grab_min(x):
-        _, dsts = pairwise_distances_argmin_min(np.array([x]),
-                                                Y,
-                                                axis=1,
-                                                metric=mixed_metric,
-                                                metric_kwargs={"is_bool_feature": bool_mask}
-                                                )
-        return dsts
-    min_dsts_ = np.hstack(Pool().map(grab_min, X.tolist()))
-    return min_dsts_
+def min_distance(features_1_list, features_2_array, cat_weight_, bool_features=None):
+    features_1 = np.array(features_1_list)
+    min_dist = np.inf
+    for features_2 in features_2_array.tolist():
+        dist = mixed_metric(features_1, np.array(features_2), cat_weight_, bool_features)
+        if dist < min_dist:
+            min_dist = dist
+    return min_dist
 
 
-def min_distances(X, Y, bool_mask):
-    """
-    Serial computation of minimum distances.
-    :param X: 2D numpy array of features
-    :param Y: 2D numpy array of features
-    :param bool_mask: 1D numpy boolean array indexing which features are bool type (fingerprints)
-    :return numpy array of minimum distance from each row of X to any row of Y
-    """
-    _, min_dsts_ = pairwise_distances_argmin_min(X, Y, axis=1, metric=mixed_metric,
-                                                 metric_kwargs={"is_bool_feature": bool_mask}
-                                                 )
-    return min_dsts_
+def min_distances(features_1, features_2, cat_weight_=None, bool_features=None):
+    if bool_features is not None and cat_weight_ is None:
+        cat_weight_ = np.mean(bool_features)
+    elif bool_features is None and cat_weight_ is None:
+        cat_weight_ = 0
+    else:
+        pass
+    distances_ = [min_distance(row, features_2, cat_weight_, bool_features) for row in features_1.tolist()]
+    return np.array(distances_).flatten()
 
 
-def min_dist_func_getter(parallel_, is_bool_feature_):
+def min_distances_parallel(features_1, features_2, cat_weight_=None, bool_features=None):
+    if bool_features is not None and cat_weight_ is None:
+        cat_weight_ = np.mean(bool_features)
+    elif bool_features is None and cat_weight_ is None:
+        cat_weight_ = 0
+    else:
+        pass
+
+    def get_min_distance(features_list):
+        return min_distance(features_list, features_2, bool_features)
+    distances = np.array(Pool().map(get_min_distance, features_1.tolist()))
+    return distances.flatten()
+
+
+def min_dist_func_getter(parallel_, cat_weight_=None, is_bool_feature_=None):
     """Get correct distance function according to 'parallel' keyword, and pre-feed it with is_bool_feature_ argument."""
     if parallel_:
-        return lambda X, Y: min_distances_parallel(X, Y, is_bool_feature_)
+        return lambda X, Y: min_distances_parallel(X, Y, cat_weight_, is_bool_feature_)
     else:
-        return lambda X, Y: min_distances(X, Y, is_bool_feature_)
+        return lambda X, Y: min_distances(X, Y, cat_weight_, is_bool_feature_)
 
 
-def get_weights_and_bias(features_, is_bool_feature_, labels_, is_validation_, parallel=False):
+def get_weights_and_bias(features_, labels_, is_validation_, cat_weight=None, is_bool_feature=None, parallel=False):
     """
     Computes validation weights for later use in weighted model performance metrics. Also computes uky_score.
     :param features_: 2D numpy array of features
-    :param is_bool_feature_: 1D boolean numpy array indicating which features are binary (fingerprints)
+    :param cat_weight: Scalar between 0 and 1 for how much to weight the binary features in distance computation
+    :param is_bool_feature: 1D boolean numpy array indicating which features are binary (fingerprints)
     :param labels_: 1D boolean numpy array of activity labels
     :param is_validation_: 1D boolean numpy array indicating which rows are in the validation set
     :param parallel: Optional keyword argument (Default: False) for parallel computation of distances
     :return: 1D numpy array of weights (length equal to number of validation rows), uky_score (float)
     """
-    training_indices = np.where(~is_validation_)[0]
-    validation_indices = np.where(is_validation_)[0]
+    training_indices, *_ = np.where(~is_validation_)
+    validation_indices, *_ = np.where(is_validation_)
     validation_size = validation_indices.size
-    active_indices = np.where(labels_)[0]
-    decoy_indices = np.where(~labels_)[0]
+    active_indices, *_ = np.where(labels_)
+    decoy_indices, *_ = np.where(~labels_)
     training_active_indices = np.sort(np.array(list(set(training_indices).intersection(set(active_indices)))))
     training_decoy_indices = np.sort(np.array(list(set(training_indices).intersection(set(decoy_indices)))))
     validation_decoy_indices = np.sort(np.array(list(set(validation_indices).intersection(set(decoy_indices)))))
     validation_active_indices = np.sort(np.array(list(set(validation_indices).intersection(set(active_indices)))))
+    if is_bool_feature is None:
+        "If no binary features are flagged, assume all features are continuous."
+        *_, dim = features_.shape
+        is_bool_feature = np.zeros(dim).astype(bool)
     # compute min distances
-    min_dist_func = min_dist_func_getter(parallel, is_bool_feature_)
+    min_dist_func = min_dist_func_getter(parallel, is_bool_feature)
     active_active_dsts = min_dist_func(features_[validation_active_indices], features_[training_active_indices])
     active_decoy_dsts = min_dist_func(features_[validation_active_indices], features_[training_decoy_indices])
     decoy_active_dsts = min_dist_func(features_[validation_decoy_indices], features_[training_active_indices])
